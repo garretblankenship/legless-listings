43936214b937a4aee6922f527b605b80822d02f9
garretblankenship <garret.blankenship@coderacademy.edu.au>
2019-04-18 15:11:17 +1000

    activated active storage

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -54,6 +54,6 @@ class ListingsController < ApplicationController
     end
 
     def listing_params
-        params.require(:listing).permit(:title, :description, :breed_id, :sex, :price, :deposit, :date_of_birth, :diet)
+        params.require(:listing).permit(:title, :description, :breed_id, :sex, :price, :deposit, :date_of_birth, :diet, :picture)
     end
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,5 @@ class Listing < ApplicationRecord
   belongs_to :breed
   enum sex: { female: 0, male: 1 }
   validates :title, :breed_id, :sex, :price, :deposit, :date_of_birth, presence: true
+  has_one_attached :picture
 end
