66c22b1a926ec82227dcf27bc4178134048cea75
garretblankenship <garret.blankenship@coderacademy.edu.au>
2019-04-18 12:40:33 +1000

    added in logic for create method

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,10 +1,17 @@
 class ListingsController < ApplicationController
     before_action :set_listing, only: [:show, :edit, :update, :destroy]
-    
+    before_action :set_breeds_and_sexes, only: [:new, :edit]
+
     def create
         #create new listing
         @listing = Listing.create(listing_params)
-        byebug
+        
+        if @listing.errors.any?
+            set_breeds_and_sexes
+            render "new"
+        else
+            redirect_to listings_path
+        end
     end
 
 
@@ -28,8 +35,6 @@ class ListingsController < ApplicationController
     def new
         #shows form for creating a new listing
         @listing = Listing.new
-        @breeds = Breed.all
-        @sexes = Listing.sexes.keys
     end
 
     def show
@@ -38,6 +43,11 @@ class ListingsController < ApplicationController
 
     private
 
+    def set_breeds_and_sexes
+        @breeds = Breed.all
+        @sexes = Listing.sexes.keys
+    end
+    
     def set_listing
         id = params[:id]
         @listing = Listing.find(id)
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,5 @@
 class Listing < ApplicationRecord
   belongs_to :breed
   enum sex: { female: 0, male: 1 }
+  validates :title, :breed_id, :sex, :price, :deposit, :date_of_birth, presence: true
 end
